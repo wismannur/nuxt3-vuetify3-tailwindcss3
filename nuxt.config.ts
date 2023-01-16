69b4b13583ba89f5a480f6619ab678e962c1a1aa
wismannur <wismannura@gmail.com>
2023-01-16 11:12:54 +0700

    setup vuetify 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/css/tailwind.css"],
+  css: [
+    "~/assets/css/tailwind.css",
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
