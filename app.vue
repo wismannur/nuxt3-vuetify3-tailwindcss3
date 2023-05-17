526c394ca2fcfb399a8cd80857bc86c8d99d8109
wismannur <wismannura@gmail.com>
2023-05-17 17:37:36 +0700

    upgrade to nuxt 3.5.0

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="home">
-    <h1 class="text-3xl font-bold underline text-teal-500">Hello world! {{ runtimeConfig.public.lmsEnv }}</h1>
+    <h1 class="text-3xl font-bold underline text-teal-500">Hello world! {{ runtimeConfig.public.projectEnv }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
 
-console.log(runtimeConfig.apiSecret);
-console.log(runtimeConfig.public.apiBase);
-console.log(runtimeConfig.public.lmsEnv);
+console.log('runtimeConfig ', runtimeConfig)
+console.log(`runtimeConfig.public.projectEnv ${runtimeConfig.public.projectEnv}`);
+console.log(`runtimeConfig.public.hostAPI ${runtimeConfig.public.hostAPI}`);
 </script>
